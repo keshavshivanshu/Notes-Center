d5471d18b297cfe470a84acc23a7bc4d7cc2f680
keshavshivanshu <keshavkumarshivanshu3@gmail.com>
2022-09-29 12:46:56 +0530

    pushing the notes center

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+const { application } = require('express');
+const express = require('express');
+const dotenv = require("dotenv");
+const notes = require('./data/notes');
+const userRoutes = require('./routes/userRoutes')
+const noteRoutes = require('./routes/noteRoutes');
+const connectDB = require('./config/db');
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
+const path = require("path");
+
+const app = express();
+dotenv.config();
+connectDB();
+app.use(express.json());
+
+
+//app.get('/api/notes',(req,res) => {
+  //  res.json(notes);
+//});
+
+app.use('/api/users',userRoutes);
+app.use('/api/notes',noteRoutes);
+
+//deployment
+__dirname = path.resolve();
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'/frontend/build')));
+
+    app.get('*',(req,res) => {
+      res.sendFile(path.resolve(__dirname,"frontend","build","index.html"));
+    })
+}else{
+  app.get('/',(req,res) => {
+    res.send("API is running");
+  });
+}
+
+
+app.use(notFound);
+app.use(errorHandler);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,console.log(`SERVER STARTED ON PORT NO: ${PORT}`));
